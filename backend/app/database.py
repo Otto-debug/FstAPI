cec6caaa8da55bb35c837b94d7f519369f95347a
Otto-debug <ottolumbert@gmail.com>
2025-05-30 10:22:12 +0300

    Подключение к базе данных через SQLAlchemy

---
+++ backend/app/database.py
@@ -0,0 +1,10 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+import os
+
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@db:5432/postgres")
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+
+Base = declarative_base()
